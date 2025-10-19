0b33f1cfb9045059efd902bd8ca9974f4443c413
hridoytv4melcom-beep <hridoytv.4mel.com@gmail.com>
2025-10-19 11:07:29 +0600

    Refactor handleRequest function and update headers

--- worker.js
+++ worker.js
@@ -6,8 +6,6 @@ async function handleRequest(request) {
   const url = new URL(request.url)
 
   let originalURL = ''
-
-  // চ্যানেল ম্যাপিং
   if (url.pathname.endsWith('/sony-aath.m3u8')) {
     originalURL = 'https://live20.bozztv.com/giatvplayout7/giatv-209611/tracks-v1a1/mono.ts.m3u8'
   } else if (url.pathname.endsWith('/srk-tv.m3u8')) {
@@ -19,7 +17,8 @@ async function handleRequest(request) {
   try {
     const response = await fetch(originalURL, {
       headers: {
-        'User-Agent': 'Mozilla/5.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Referer': 'https://www.google.com'
       },
     })
 
@@ -28,10 +27,7 @@ async function handleRequest(request) {
     headers.set('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS')
     headers.set('Access-Control-Allow-Headers', '*')
 
-    return new Response(response.body, {
-      status: response.status,
-      headers,
-    })
+    return new Response(response.body, { status: response.status, headers })
   } catch (err) {
     return new Response('Stream Fetch Error: ' + err.message, { status: 502 })
   }
